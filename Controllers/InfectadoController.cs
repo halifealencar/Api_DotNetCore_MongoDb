a6f09784c9c00334c6317a9edcd14c4be7218726
Halife <halife.alencar@gmail.com>
2020-12-16 21:20:09 -0400

    first commit

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,39 @@
+using Api.Data.Collections;
+using Api.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoViewModel infectadoVM)
+        {
+            var infectado = new Infectado(infectadoVM.DataNascimento, infectadoVM.Sexo, infectadoVM.Latitude, infectadoVM.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+            
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            
+            return Ok(infectados);
+        }
+    }
+}
---
+++ ViewModel/InfectadoViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Api.ViewModel
+{
+    public class InfectadoViewModel
+    {
+        public DateTime DataNascimento { get; set; }
+
+        public string Sexo { get; set; }
+
+        public double Latitude { get; set; }
+        
+        public double Longitude { get; set; }
+    }
+}
